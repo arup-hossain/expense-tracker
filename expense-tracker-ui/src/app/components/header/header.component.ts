db1d7e4bde54953953fbc793fb7429a5c861aef5
Arup Hossain <aruphossain29@gmail.com>
2021-05-22 18:55:42 -0400

    logout and save token in localstorage

--- expense-tracker-ui/src/app/components/header/header.component.ts
+++ expense-tracker-ui/src/app/components/header/header.component.ts
@@ -1,14 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    authenticated: boolean = false;
+    authSub: Subscription;
 
-    ngOnInit(): void { }
+    constructor(private authService: AuthService) { }
+
+    ngOnInit(): void {
+        this.checkAuth();
+    }
+
+    checkAuth(): void {
+        this.authService.getAuthObs().subscribe(res => this.authenticated = res);
+        this.authService.triggerAuthObs();
+    }
+
+    ngOnDestroy(): void {
+        this.authSub.unsubscribe();
+    }
+
+    logout(): void {
+        this.authService.logout();
+    }
 
 }
--- expense-tracker-ui/src/app/services/auth.service.ts
+++ expense-tracker-ui/src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,17 +10,49 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
 
     private apiUrl: string = environment.apiUrl + '/auth';
+    private token: string;
+    private authSubject: Subject<boolean> = new Subject<boolean>();
 
-    constructor(private http: HttpClient) { }
+    constructor(
+        private http: HttpClient,
+        private router: Router) { }
 
     register(data: { email: string, password: string }): void {
         this.http.post<string>(this.apiUrl + '/register', data).subscribe(
-            res => console.log(res));
+            res => this.handleLogin(res));
     }
 
     login(data: { email: string, password: string }): void {
         this.http.post<string>(this.apiUrl + '/login', data).subscribe(
-            res => console.log(res));
+            res => this.handleLogin(res));
+    }
+
+    handleLogin(token: string): void {
+        this.token = token;
+        this.triggerAuthObs();
+        localStorage.setItem('token', token);
+        this.router.navigate(['/']);
+    }
+
+    getAuthObs(): Observable<boolean> {
+        return this.authSubject.asObservable();
+    }
+
+    triggerAuthObs(): void {
+        this.authSubject.next(this.token !== undefined);
+    }
+
+    logout(): void {
+        this.token = undefined;
+        this.triggerAuthObs();
+        localStorage.removeItem('token');
+        this.router.navigate(['/login']);
+    }
+
+    checkForToken(): void {
+        const token: string = localStorage.getItem('token');
+        if (!token) return;
+        this.handleLogin(token);
     }
 
 }
