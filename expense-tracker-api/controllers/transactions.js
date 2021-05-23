c9b69bf7df33a12483da869d82a035ad04681411
Arup Hossain <aruphossain29@gmail.com>
2021-05-23 11:42:40 -0400

    connect transactions and users

--- expense-tracker-api/controllers/transactions.js
+++ expense-tracker-api/controllers/transactions.js
@@ -3,12 +3,13 @@ const createError = require('http-errors');
 const Transaction = require('../models/Transaction');
 
 exports.createTransaction = asyncHandler(async (req, res, next) => {
+    req.body.createdBy = req.userId;
     const transaction = await Transaction.create(req.body);
     res.status(201).json(transaction);
 });
 
 exports.getTransactions = asyncHandler(async (req, res, next) => {
-    const transactions = await Transaction.find();
+    const transactions = await Transaction.find({ createdBy: req.userId });
     res.status(200).json(transactions);
 });
 
@@ -18,11 +19,19 @@ exports.getTransaction = asyncHandler(async (req, res, next) => {
 });
 
 exports.updateTransaction = asyncHandler(async (req, res, next) => {
-    const transaction = await Transaction.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const transaction = await Transaction.findById(req.params.id);
+    if (!transaction.createdBy.equals(req.userId)) {
+        return next(createError(403, 'You cannot update this transaction'));
+    }
+    await transaction.updateOne(req.body);
     res.status(200).json(transaction);
 });
 
 exports.deleteTransaction = asyncHandler(async (req, res, next) => {
-    const transaction = await Transaction.findByIdAndDelete(req.params.id);
+    const transaction = await Transaction.findById(req.params.id);
+    if (!transaction.createdBy.equals(req.userId)) {
+        return next(createError(403, 'You cannot update this transaction'));
+    }
+    await transaction.deleteOne();
     res.status(200).json(transaction);
 });
--- expense-tracker-api/models/Transaction.js
+++ expense-tracker-api/models/Transaction.js
@@ -9,7 +9,12 @@ const transactionSchema = mongoose.Schema({
         type: Date,
         required: true
     },
-    note: String
+    note: String,
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Transaction', transactionSchema);
