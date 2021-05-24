7e79883bc832efe8680a6322db212b78ad7f5a7a
Arup Hossain <aruphossain29@gmail.com>
2021-05-24 02:43:09 -0400

    create default categories on register

--- expense-tracker-api/models/User.js
+++ expense-tracker-api/models/User.js
@@ -11,4 +11,18 @@ const userSchema = mongoose.Schema({
     }
 });
 
+userSchema.post('save', async function () {
+    this.model('Category').create({ name: 'Bills', type: 'Expense', createdBy: this });
+    this.model('Category').create({ name: 'Food', type: 'Expense', createdBy: this });
+    this.model('Category').create({ name: 'Education', type: 'Expense', createdBy: this });
+    this.model('Category').create({ name: 'Car', type: 'Expense', createdBy: this });
+    this.model('Category').create({ name: 'Shopping', type: 'Expense', createdBy: this });
+    this.model('Category').create({ name: 'Friends', type: 'Expense', createdBy: this });
+    this.model('Category').create({ name: 'Retirement', type: 'Expense', createdBy: this });
+    this.model('Category').create({ name: 'Other', type: 'Expense', createdBy: this });
+    this.model('Category').create({ name: 'Salary', type: 'Income', createdBy: this });
+    this.model('Category').create({ name: 'Side Hustle', type: 'Income', createdBy: this });
+    this.model('Category').create({ name: 'Other', type: 'Income', createdBy: this });
+});
+
 module.exports = mongoose.model('User', userSchema);
